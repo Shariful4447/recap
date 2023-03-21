e4ef3e1a86c2a70cd9a9522d69f25e0b6614288e
Shariful4447 <66253663+Shariful4447@users.noreply.github.com>
2023-03-21 15:40:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,6 +11,7 @@ function App() {
       <Nayok name={nayoks[0]}></Nayok>
       <Nayok name={nayoks[2]}></Nayok>
       <Nayok name={nayoks[3]}></Nayok>
+      <MovieCounter></MovieCounter>
 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -22,7 +25,22 @@ function Nayok(props){
   return(<div style={{color:'red', border:'2px solid blue'}}>
     <h1>Ami naayok: {props.name}</h1>
     <h3>i have done 5 movies in {props.age || 30}years</h3>
+    
   </div>)
+}
+function MovieCounter(){
+  let [count, setCount] = useState(1);
+  const handleClick=()=>setCount(count+1);
+  return(
+    <div>
+      <button onClick={handleClick}>ADD Movie</button>
+      <h3>Total Number of movies :{count}</h3>
+      <MovieDisplay movies={count}></MovieDisplay>
+    </div>
+  )
+}
+function MovieDisplay(props){
+  return(<h3>Movies i have Acteed :{props.movies}</h3>)
 }
 
 export default App;
